6652bf47c64435ffa99858607297d55d96e3a195
minerkg <129666850+minerkg@users.noreply.github.com>
2025-06-09 21:02:16 +0300

    book model classses created

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace publisher_app.Models
+{
+    public class Book
+    {
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        public int? Year { get; set; }
+        public double? Price { get; set; }
+    }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace publisher_app.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<PublisherContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; } = null!;
+
+
+
+    }
+}
