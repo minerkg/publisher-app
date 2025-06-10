41cb7534f85e72b3c5d39372c1673f8969ede08c
minerkg <129666850+minerkg@users.noreply.github.com>
2025-06-10 21:46:50 +0300

    Publisher added to the book

--- Models/Book.cs
+++ Models/Book.cs
@@ -5,7 +5,7 @@
         public long Id { get; set; }
         public string? Title { get; set; }
         public string? Author { get; set; }
-        public string? Publisher { get; set; }
+        public Publisher? Publisher { get; set; }
         public int? Year { get; set; }
         public double? Price { get; set; }
     }
