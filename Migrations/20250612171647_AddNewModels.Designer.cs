b24224506abcea3580db053765e93c4404163942
minerkg <129666850+minerkg@users.noreply.github.com>
2025-06-12 20:21:24 +0300

    migration code working

--- Migrations/20250612171647_AddNewModels.Designer.cs
+++ Migrations/20250612171647_AddNewModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using publisher_app.Models;
 
@@ -11,9 +12,11 @@ using publisher_app.Models;
 namespace publisher_app.Migrations
 {
     [DbContext(typeof(PublisherContext))]
-    partial class PublisherContextModelSnapshot : ModelSnapshot
+    [Migration("20250612171647_AddNewModels")]
+    partial class AddNewModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
