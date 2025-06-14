341700d62493902fcd19109462b2baf97a58f085
minerkg <129666850+minerkg@users.noreply.github.com>
2025-06-14 12:11:35 +0300

    4300 port added too to cors

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200") // Adjust to match your frontend's URL
+            policy.WithOrigins("http://localhost:4200", "http://localhost:4300") // Adjust to match your frontend's URL
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
